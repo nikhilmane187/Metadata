e44f5b7e86ad50a9a59900ffea36415ca339d9b0
Nikhil Mane <nikhilmane187@gmail.com>
2022-10-14 22:55:33 +0530

    First comitt

---
+++ precache-manifest.1401906082811d82466463a82ffd2faa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "3249c4285dbee14940f4",
+    "url": "/static/css/main.16e28f8f.chunk.css"
+  },
+  {
+    "revision": "3249c4285dbee14940f4",
+    "url": "/static/js/main.3249c428.chunk.js"
+  },
+  {
+    "revision": "662a448855c5de048ffc",
+    "url": "/static/js/1.662a4488.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7d1fe24b3072b92a2bd11f8a5d569f9b",
+    "url": "/static/media/me-about.7d1fe24b.jpg"
+  },
+  {
+    "revision": "8d278ea3fddd6336d27842e3e44d51cf",
+    "url": "/static/media/cv.8d278ea3.pdf"
+  },
+  {
+    "revision": "0a8cf10ed941810c0051a81e9a1572eb",
+    "url": "/static/media/me.0a8cf10e.png"
+  },
+  {
+    "revision": "1f121fa73056b9abbd87fc31f875a117",
+    "url": "/index.html"
+  }
+];
